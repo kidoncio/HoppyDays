32c1989a6276ee056ace5f8bd039faad11160442
Kidoncio <lucas@lapou.com.br>
2019-06-14 01:33:07 -0300

    Add Spikeman NPC

---
+++ Hazards/Harzards.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+# Group
+const GAME_STATE_GROUP: String = "GameState"
+
+# Methods
+const HURT_METHOD: String = "hurt"
+
+func _on_SpikeTop_body_entered(body):
+	get_tree().call_group(GAME_STATE_GROUP, HURT_METHOD)
