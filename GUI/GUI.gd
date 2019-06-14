a4355b1f518643b4c60be1db6b208e0af424d170
Kidoncio <lucas@lapou.com.br>
2019-06-14 05:02:12 -0300

    Add quit button

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -1,13 +1,22 @@
 extends CanvasLayer
 
-# Nodes
-const GAME_STATE_NODE: String = "GameState"
-
 func update_lives(lives_left: int):
 	if lives_left >= 0:
-		$Control/TextureRect/VBoxContainer/HBoxContainer/LifeCount.text = str(lives_left)
+		$Control/TextureRect/HBoxContainer/LifeCount.text = str(lives_left)
 
 
 func update_coins(coins: int):
 	if coins >= 0:
-		$Control/TextureRect/VBoxContainer/HBoxContainer/CoinCount.text = str(coins)
+		$Control/TextureRect/HBoxContainer/CoinCount.text = str(coins)
+
+
+func _on_MenuButton_pressed():
+	$Control/QuitDialog.show_modal(true)
+
+
+func _on_QuitDialog_confirmed():
+	get_tree().quit()
+
+
+func _on_QuitDialog_modal_closed():
+	print("BLA1")
