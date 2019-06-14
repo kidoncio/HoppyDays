52a028ec120a2c58e63ada59a68bba92e426f604
Kidoncio <lucas@lapou.com.br>
2019-06-14 02:34:15 -0300

    Ajusta para padronizar a busca das scenes colocando como constante

--- Scenes/GameOver.gd
+++ Scenes/GameOver.gd
@@ -1,7 +1,8 @@
 extends Control
 
-
+# Scenes
+const LEVEL1_SCENE: String = "res://Scenes/Levels/Level1.tscn"
 
 func _on_RestartButton_pressed():
-	get_tree().change_scene("res://Scenes/Levels/Level1.tscn")
+	get_tree().change_scene(LEVEL1_SCENE)
 	pass
