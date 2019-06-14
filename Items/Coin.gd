32968e82fd3fb638f15e18cab294161c79d43713
Kidoncio <lucas@lapou.com.br>
2019-06-14 01:32:24 -0300

    Add Item Coin

---
+++ Items/Coin.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var taken: bool = false
+
+# Groups
+const GAME_STATE_GROUP: String = "GameState"
+
+# Methods
+const COIN_UP_METHOD: String = "coin_up"
+
+func _on_Area2D_body_entered(body):
+	if !taken:
+		taken = true
+		
+		$AnimationPlayer.play("Die")
+		$AudioStreamPlayer2D.play()
+		get_tree().call_group(GAME_STATE_GROUP, COIN_UP_METHOD)
+
+func die() -> void:
+	queue_free()
