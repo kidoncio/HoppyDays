8cef73f048ad082099594bb8566c5f5cb89928ea
Kidoncio <lucas@lapou.com.br>
2019-06-16 04:06:40 -0300

    Ajusta para o Harzards só afetar caso seja o jogador

--- Hazards/Harzards.gd
+++ Hazards/Harzards.gd
@@ -7,4 +7,5 @@ const GAME_STATE_GROUP: String = "GameState"
 const HURT_METHOD: String = "hurt"
 
 func _on_SpikeTop_body_entered(body):
-	get_tree().call_group(GAME_STATE_GROUP, HURT_METHOD)
+	if body.has_method(HURT_METHOD):
+		get_tree().call_group(GAME_STATE_GROUP, HURT_METHOD)
