f6f1f979504f9894162990a5daeab4b0a1acfc31
Kidoncio <lucas@lapou.com.br>
2019-06-16 22:06:33 -0300

    Add feedback de vida ao GUI

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -14,6 +14,7 @@ const GAME_STATE_GROUP: String = "GameState"
 const RESTART_LEVEL_METHOD: String = "restart_level"
 
 func _ready():
+	hide_life_feedback()
 	$Control/Menu/SoundButton.pressed = configuration.sound_muted
 	update_sfx(configuration.sound_muted)
 	update_sound_button_text()
@@ -21,6 +22,17 @@ func _ready():
 
 func update_lives(lives_left: int):
 	if lives_left >= 0:
+		var lifesOnFeedBack: int = int($Control/TextureRect/HBoxContainer/LifeCount.text)
+		
+		if lives_left != lifesOnFeedBack:
+			var feedbackSignal: String = ""
+			
+			if lives_left > lifesOnFeedBack:
+				feedbackSignal = "+"
+			
+			$Control/LifeFeedback/HBoxContainer/LifeFeedbackLabel.text = feedbackSignal + str(lives_left - lifesOnFeedBack)
+			show_life_feedback()
+		
 		$Control/TextureRect/HBoxContainer/LifeCount.text = str(lives_left)
 
 
@@ -71,3 +83,23 @@ func _on_Menu_popup_hide():
 
 func _on_RestartLevelButton_pressed():
 	get_tree().call_group(GAME_STATE_GROUP, RESTART_LEVEL_METHOD)
+
+
+func _on_LifeFeedbackTimer_timeout():
+	hide_life_feedback()
+
+
+func show_life_feedback() -> void:
+	$Control/LifeFeedback/LifeFeedbackTimer.start()
+	
+	if $Control/LifeFeedback/HBoxContainer/LifeFeedbackLabel.visible:
+		hide_life_feedback()
+		yield(get_tree().create_timer(0.3), "timeout")
+
+	$Control/LifeFeedback/HBoxContainer/LifeFeedbackLabel.show()
+	$Control/LifeFeedback/HBoxContainer/LifeFeedbackIcon.show()
+
+
+func hide_life_feedback() -> void:
+	$Control/LifeFeedback/HBoxContainer/LifeFeedbackLabel.hide()
+	$Control/LifeFeedback/HBoxContainer/LifeFeedbackIcon.hide()
